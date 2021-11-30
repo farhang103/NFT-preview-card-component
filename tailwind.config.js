58213bc26da15d6b530b6c1c2753f19640c7c0ec
Farhang Alizadeh <77861189+farhang103@users.noreply.github.com>
2021-11-30 20:14:12 +0330

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,46 @@
+module.exports = {
+  mode: 'jit',
+  purge: [
+    './public/**/*.html'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+
+      screen: {
+        'sm': '375px',
+        'lg': '1440px'
+      },
+
+      colors: {
+        primary: {
+          soft_blue: 'hsl(215, 51%, 70%)',
+          cyan: 'hsl(178, 100%, 50%)',
+          cyan_op50: 'hsla(178, 100%, 50%, .5)'
+        },
+
+        neutral: {
+          main_very_dark_blue: 'hsl(217, 54%, 11%)', 
+          card_very_dark_blue: 'hsl(216, 50%, 16%)',
+          line_very_dark_blue: 'hsl(215, 32%, 27%)',
+          white: 'hsl(0, 0%, 100%)'
+        }
+      },
+
+      fontFamily: {
+        'outfit': "'outfit', sans-serif"
+      },
+
+      spacing: {
+        '15': '3.875rem',
+        '4.5': '1.125rem',
+        '70': '17.5rem',
+        '0.25': '0.0625rem'
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
